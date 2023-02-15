dbcbf54f0faa278953413187a599b1c71cfa391b
Agailh <agailham86@gmail.com>
2023-02-15 16:10:57 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { BsPerson } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center items-center h-20 px-4 ">
+    <div className="flex justify-between items-center h-20 px-4 ">
       <div>
         <h1>Lemao Beaches</h1>
       </div>
-      <ul className="flex">
+      <ul className=" hidden md:flex">
         <li>Home</li>
         <li>Destination</li>
         <li>Travel</li>
         <li>Home</li>
         <li>Home</li>
       </ul>
-      <div className="flex">
-        <BiSearch />
-        <BsPerson />
+      <div className="hidden md:flex">
+        <BiSearch size={20} />
+        <BsPerson size={20} />
+      </div>
+
+      <div>
+        <HiOutlineMenuAlt4 />
       </div>
     </div>
   );
