5804266a2ae6d5643fbdaf46ab18ea56881b64c6
Agailh <agailham86@gmail.com>
2023-02-15 13:00:15 +0700

    nav bar init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center items-center h-20 ">
+      <div>
+        <h1>Lemao Beaches</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
