b3901a8d033a2074036e110a4334c8d6b327c038
Agailh <agailham86@gmail.com>
2023-02-15 15:36:12 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { BsPerson } from "react-icons/bs";
+import { BiSearch } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center items-center h-20 ">
+    <div className="flex justify-center items-center h-20 px-4 ">
       <div>
         <h1>Lemao Beaches</h1>
+      </div>
+      <ul className="flex">
+        <li>Home</li>
+        <li>Destination</li>
+        <li>Travel</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+      <div className="flex">
+        <BiSearch />
+        <BsPerson />
       </div>
     </div>
   );
