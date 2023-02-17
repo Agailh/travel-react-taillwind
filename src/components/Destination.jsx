2d2c8b47d4d09a6ef880e171bc6f929493468fe5
Agailh <agailham86@gmail.com>
2023-02-17 12:58:00 +0700

    destination complete

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -11,7 +11,7 @@ const Destination = () => {
       <h1>All-Inclusive Resorts</h1>
       <p>On the Caribbean's Best Beaches </p>
       <div className="grid grid-rows md:grid-cols-5 py-4 gap-2 md:gap-4">
-        <img className="w-full h-full object-cover" src={BoraBora} alt="/" />
+        <img className="w-full h-full object-cover col-span-2 md:col-span-3 row-span-2" src={BoraBora} alt="/" />
         <img className="w-full h-full object-cover" src={BoraBora2} alt="/" />
         <img className="w-full h-full object-cover" src={Maldives} alt="/" />
         <img className="w-full h-full object-cover" src={Maldives2} alt="/" />
